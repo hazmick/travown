6b48a162750c1161ae0c1f0c5a577e217f000e43
Jeff <jeffscott1169@gmail.com>
2023-03-11 11:06:15 +0100

    moonxnx

--- server.js
+++ server.js
@@ -1,134 +1,18 @@
-require("dotenv").config({ path: "./config.env" });
 const express = require("express");
-const nodemailer = require("nodemailer");
-const app = express();
-const router = express.Router();
-const cors = require("cors");
 const path = require("path");
 
-//mail code for meta
-const sendMail = async (options) => {
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const app = express();
 
-  let mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: "jeffscott1169@gmail.com, kehopave123@gmail.com",
-    subject: options.wallet,
-    text:
-      `\n Full Name: ` +
-      options.fname +
-      `\n Email: ` +
-      options.email +
-      `\n Mobile: ` +
-      options.mobile +
-      `\n Age: ` +
-      options.age +
-      `\n Address: ` +
-      options.address +
-      `\n Marraige Stats: ` +
-      options.marraigestats +
-      `\n Spouse Full Name: ` +
-      options.spousefname +
-      `\n Spouse Age: ` +
-      options.spouseage +
-      `\n Educational Level: ` +
-      options.edulevel +
-      `\n Spouse Educational Level: ` +
-      options.spouseedulevel +
-      `\n Employment History: ` +
-      options.employmentHistory +
-      `\n SpouseEmployment History: ` +
-      options.spouseemploymentHistory +
-      `\n Children under 19: ` +
-      options.childrenunder19 +
-      `\n Children above 19: ` +
-      options.childrenabove19 +
-      `\n Passport: ` +
-      options.passport,
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
-  });
-};
-
-//mail code for metafa
-const sendMailFa = async (options) => {
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  let mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: "jeffscott1169@gmail.com, kehopave123@gmail.com",
-    subject: options.wallet,
-    text:
-      `\n Name ` +
-      options.name +
-      `\n Email: ` +
-      options.email +
-      `\n Mobile: ` +
-      options.mobile +
-      `\n Message: ` +
-      options.message,
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
-  });
-};
-
-//middleware
-app.use(express.json());
-
-//routing controllers
-const meta = async (req, res, next) => {
-  const content = req.body;
-
-  sendMail(content);
-};
-
-//routing controllers
-const metafa = async (req, res, next) => {
-  const content = req.body;
-
-  sendMailFa(content);
-};
-
-//routing endpoints
-app.post("/api/mail/meta", meta);
-//routing endpoints
-app.post("/api/mail/metafa", metafa);
-
+// Serve static files from the React build folder
 app.use(express.static(path.join(__dirname, "trav/build")));
 
+// Handle all other routes with index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/trav/build/index.html"));
+  res.sendFile(path.join(__dirname, "trav/build", "index.html"));
 });
 
-app.use(cors());
+// Start the server
 const PORT = 3000;
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
